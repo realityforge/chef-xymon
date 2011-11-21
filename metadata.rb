b9ffccf34689d54167c595a1fba469cd752a2644
Peter Donald <peter@realityforge.org>
2011-11-21 12:10:48 +1100

    Rework the xymon recipe so that the client and server cna be stored on the same server

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+maintainer       "Fire Information Systems Group"
+maintainer_email "peter@realityforge.org"
+license          "Apache 2.0"
+description      "Installs/Configures Xymon monitoring infrastructure"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.3"
+
+attribute "xymon/servers",
+  :display_name => "Xymon Server ip addresses",
+  :description => "List of Xymon servers to send monitoing information to",
+  :type => "array",
+  :default => ""
---
+++ recipes/client.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook Name:: xymon
+# Recipe:: default
+#
+# Copyright 2011, Fire Information Systems Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+raise "Unexpected platform" unless node.platform == "ubuntu"
+
+package "xymon-client" do
+  action :install
+end
+
+service "hobbit-client" do
+  supports :restart => true, :reload => true, :status => true
+  action [:enable, :start]
+end
+
+template "/etc/default/hobbit-client" do
+  source "hobbit-client.erb"
+  owner 'hobbit'
+  group 'hobbit'
+  mode "0644"
+  notifies :restart, resources(:service => "hobbit-client")
+end
