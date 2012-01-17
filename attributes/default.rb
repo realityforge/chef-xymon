aa22c0334d195b4f112dd9fa712e6ed35099b9e0
Peter Donald <peter@realityforge.org>
2012-01-17 14:01:51 +1100

    Cleanup the copyright banners

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,5 @@
 #
-# Cookbook Name:: xymon
-# Attributes:: default
-#
-# Copyright 2011, Fire Information Systems Group
+# Copyright 2011, Peter Donald
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
